4856e78e9c8745a192685d1b363f0083274e8b1f
ST <A>
2023-08-06 20:34:38 +0900

    とりあえず作成完了。 違う図形や、3Dなどでも作ってみたい

--- Assets/Prefub/PendulumWave.cs
+++ Assets/Prefub/PendulumWave.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 using static UnityEditor.PlayerSettings;
 
 public class PendulumWave : MonoBehaviour
@@ -16,20 +18,31 @@ public class PendulumWave : MonoBehaviour
     public int min_radius;
     public Transform positionCircle;    //‘S‰~‰^“®‚Ì’†S
 
-    private float speed = 20f; // ‰~‰^“®‚Ì‘¬‚³
+    public GameObject Line;
+    GameObject[] Line_arrg = new GameObject[5];
+
+    public float speed; // ‰~‰^“®‚Ì‘¬‚³
+    public float speed_magnification;
 
     // Start is called before the first frame update
     void Start()
     {
+        if(circle_num>10)
+        {
+            //‰æ–Ê‚É“ü‚é‚Ì‚ª10ŒÂ‚Ü‚Å
+            circle_num = 10;
+        }
         Circle_num = circle_num;
         Max_radius = max_radius;
         Min_radius = min_radius;
 
         System.Array.Resize(ref Circle_arrg, Circle_num);
+        System.Array.Resize(ref Line_arrg, Circle_num-1);
 
         int Circle_range = Max_radius - Min_radius;
         float C_r = (Circle_range);
         float Circle_distance = C_r / Circle_num;
+        int Circle_color = 255 / Circle_num;
         if (Max_radius<=Min_radius)
         {
 
@@ -49,8 +62,22 @@ public class PendulumWave : MonoBehaviour
             string A;
             A = "Circle" + i.ToString("0");
             obj.name = A;
+            //F‚Ì•ÏXˆ—
+            byte byteValue_Red = System.Convert.ToByte(Circle_color * i);
+            byte byteValue_Blue = System.Convert.ToByte(255- byteValue_Red);
+            obj.GetComponent<SpriteRenderer>().material.color = new Color32(byteValue_Blue, 0, byteValue_Red, 255);
+
             Circle_arrg[i] = obj;
             Debug.Log(Circle_arrg[i].name);
+            if(i<Circle_num-1)
+            {
+                GameObject obj_line;
+                obj_line = Instantiate(Line);
+                LineRenderer line = obj_line.GetComponent<LineRenderer>();
+                line.positionCount = 2;
+
+                Line_arrg[i] = obj_line;
+            }
 
         }
 
@@ -60,8 +87,16 @@ public class PendulumWave : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Circle_arrg[0].transform.RotateAround(positionCircle.position, Vector3.forward, speed * Time.deltaTime);
-        Circle_arrg[4].transform.RotateAround(positionCircle.position, Vector3.forward, speed * Time.deltaTime);
-        Circle_arrg[9].transform.RotateAround(positionCircle.position, Vector3.forward, speed * Time.deltaTime);
+        for(int i=0;i< Circle_arrg.Length;i++)
+        {
+            Circle_arrg[i].transform.RotateAround(positionCircle.position, Vector3.forward, (i+1)*speed_magnification * speed * Time.deltaTime);
+        }
+       
+        for (int i=0;i< Line_arrg.Length; i++)
+        {
+            LineRenderer line = Line_arrg[i].GetComponent<LineRenderer>();
+            line.SetPosition(0, Circle_arrg[i].transform.position);
+            line.SetPosition(1, Circle_arrg[i+1].transform.position);
+        }
     }
 }
